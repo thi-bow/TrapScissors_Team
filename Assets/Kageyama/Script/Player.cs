d49eb42a5afe7ff94904f61ab98b2ca9e4b1fe97
thi-bow <ground_thi_bo@yahoo.co.jp>
2017-04-15 16:43:50 +0900

    プレイヤーのスクリプトを直しました。下記のことをできるようになっています。 ・Zボタンで、何かに触れていると鋏む、何にも触れてないと音を鳴らす、挟まっているときに長押しで離れる、敵が暴れているときは連打で耐える ・SetFallで落ちるまでの時間をセット ・SetBladeSpillで刃こぼれの値をセット、BladeSpillAddで刃こぼれの回復、BladeSpillSubで刃こぼれをさせる ・EndureGameで、敵が暴れているときの耐えている値を取得 以上です。

--- Assets/Kageyama/Script/Player.cs
+++ Assets/Kageyama/Script/Player.cs
@@ -1,4 +1,11 @@
-﻿using UnityEngine;
+﻿/*******************************************
+制　作　者：影山清晃
+最終編集者：影山清晃
+
+最終更新日：2017/04/15
+
+********************************************/
+using UnityEngine;
 using System.Collections;
 
 public class Player : MonoBehaviour
@@ -7,7 +14,8 @@ public class Player : MonoBehaviour
     public enum State
     {
         Wait,
-        Touch
+        Touch,
+        Endure,
     }
     public State _state;
 
@@ -21,6 +29,22 @@ public class Player : MonoBehaviour
     private Vector3 offset = Vector3.zero;
     //鋏んだときの反動中かどうか
     public bool _recoil;
+    //離すまでの時間を計測する値
+    private float _separateTime;
+    [SerializeField, TooltipAttribute("離すまでの時間")]
+    private float _separate;
+    [TooltipAttribute("耐えてる最中のゲージの値")]
+    public float _endureGage;
+    [SerializeField, TooltipAttribute("ゲージの上がる値")]
+    private float _gageUp;
+    [TooltipAttribute("刃こぼれの値")]
+    public float _bladeSpill;
+    //落ちるまでの時間を計測する値
+    private float _fallTime;
+    [TooltipAttribute("落ちるまでの時間")]
+    public float _fall;
+
+
     #endregion
 
     #region 移動や重力に必要な変数
@@ -40,6 +64,10 @@ public class Player : MonoBehaviour
         _recoil = false;
         _state = State.Wait;
         _groundOn = false;
+        _separateTime = 0;
+        _endureGage = 0;
+        _bladeSpill = 4;
+        _fallTime = 0;
     }
 	
 	// Update is called once per frame
@@ -62,7 +90,7 @@ public class Player : MonoBehaviour
     {
         Vector2 newPosition = transform.position;
         //何かを挟んでいたら、位置を固定する
-        if (_state == State.Touch)
+        if (_state != State.Wait)
         {
             _gadd = 0;
             newPosition.x = _target.transform.position.x + offset.x;
@@ -79,6 +107,7 @@ public class Player : MonoBehaviour
 
     }
 
+    //ボタンを使った操作
     void Action()
     {
         //何も鋏んでいないときに挟もうとすると音を鳴らす
@@ -93,27 +122,66 @@ public class Player : MonoBehaviour
                 SoundManger.Instance.PlaySE(1);
             }
         }
-        //ターゲットを外す
-        if ((Input.GetKeyDown(KeyCode.Z) || Input.GetKeyDown(KeyCode.X)) && _recoil == false)
+
+        //何かを挟んでいるときは離す
+        else if (_state == State.Touch)
         {
-            _target = null;
-            _state = State.Wait;
+            //ボタンを離したら、リセットする
+            if(Input.GetKeyUp(KeyCode.Z))
+            {
+                _separateTime = 0;
+                return;
+            }
+            if (Input.GetKey(KeyCode.Z))
+            {
+                _separateTime += Time.deltaTime;
+            }
+            //一定時間ボタンを押していたら、離す
+            if (_separateTime >= _separate)
+            {
+                _target = null;
+                _state = State.Wait;
+                return;
+            }
+        }
+
+        //相手が暴れているときは耐える
+        else if(_state == State.Endure)
+        {
+            if(Input.GetKeyDown(KeyCode.Z))
+            {
+                _endureGage += _gageUp * _bladeSpill;
+            }
+            if(_endureGage >= 100)
+            {
+                _state = State.Touch;
+                _endureGage = 0;
+                _fallTime = 0;
+            }
+            _fallTime += Time.deltaTime;
+            if(_fallTime >= _fall)
+            {
+                _target = null;
+                _state = State.Wait;
+                _endureGage = 0;
+                _fallTime = 0;
+            }
         }
 
 
     }
 
+    //当たっている最中も取得する当たり判定
     void OnTriggerStay2D(Collider2D col)
     {
+        //待機状態じゃないと、衝突しても無視する
+        if (_state != State.Wait) return; 
         //何かとぶつかっているときにZボタンを押すと大きいハサミで鋏む
         if (Input.GetKeyDown(KeyCode.Z) && _target == null &&
             col.tag != "Ground" && col.tag != "Gimmick")
         {
             _target = col.gameObject;
-            _recoil = true;
-            offset = transform.position - _target.transform.position;
-            _state = State.Touch;
-            StartCoroutine(Recoil());
+            Trigger();
         }
 
         //何かとぶつかっているときにXボタンを押すと小さいハサミで鋏む
@@ -121,10 +189,7 @@ public class Player : MonoBehaviour
             col.tag != "Ground")
         {
             _target = col.gameObject;
-            _recoil = true;
-            offset = transform.position - _target.transform.position;
-            _state = State.Touch;
-            StartCoroutine(Recoil());
+            Trigger();
         }
 
         if (col.tag == "Ground")
@@ -134,6 +199,7 @@ public class Player : MonoBehaviour
         }
     }
     
+    //当たり判定が外れたとき
     void OnTriggerExit2D(Collider2D col)
     {
         //挟まらずに離れたらターゲットを外す
@@ -148,10 +214,70 @@ public class Player : MonoBehaviour
         }
     }
 
+    public void Trigger()
+    {
+        _recoil = true;
+        offset = transform.position - _target.transform.position;
+        _state = State.Endure;
+        StartCoroutine(Recoil());
+    }
+
     //鋏んだら少しの間反動で抜けないようにする
     IEnumerator Recoil()
     {
         yield return new WaitForSeconds(0.2f);
         _recoil = false;
+    }
+
+    /// <summary>
+    /// 落ちるまでの時間を設定
+    /// </summary>
+    /// <param name="time">落ちるまでの時間(秒)</param>
+    public void SetFall(float time)
+    {
+        _fall = time;
+    }
+
+    /// <summary>
+    /// 刃こぼれの値を任意の値にセットする
+    /// </summary>
+    /// <param name="speed"></param>
+    public void SetBladeSpill(float bladeSpill)
+    {
+        _bladeSpill = bladeSpill;
+        //値を1未満、4より大きくしない
+        Mathf.Clamp(_bladeSpill, 1, 4);
+    }
+
+    /// <summary>
+    /// 刃こぼれを回復する
+    /// </summary>
+    /// <param name="add">回復する値</param>
+    public void BladeSpillAdd(float add)
+    {
+        _bladeSpill += add;
+        //値を1未満、4より大きくしない
+        Mathf.Clamp(_bladeSpill, 1, 4);
+        
+    }
+
+    /// <summary>
+    /// 刃こぼれをさせる
+    /// </summary>
+    /// <param name="sub">刃こぼれする値</param>
+    public void BladeSpillSub(float sub)
+    {
+        _bladeSpill -= sub;
+        //値を1未満、4より大きくしない
+        Mathf.Clamp(_bladeSpill, 1, 4);
+    }
+
+    /// <summary>
+    /// どのくらい耐えているかの値を返す
+    /// </summary>
+    /// <returns></returns>
+    public float EndureGage()
+    {
+        return _endureGage;
     }
 }
