08ac22f021259107012ff69e81cf3b2da3ab99be
Suzuki Ukyo <togingham@gmail.com>
2017-04-18 09:47:30 +0900

    item変更

--- Assets/Suzuki/Item/Program/garge.cs
+++ Assets/Suzuki/Item/Program/garge.cs
@@ -2,32 +2,39 @@
 using System.Collections;
 using UnityEngine.UI;
 
-
 public class garge : MonoBehaviour {
 
     private Player player;
+    public Image g_bar; //ゲージ
+    private RectTransform hoge;
 
-    public Image g_bar;
-
-	// Use this for initialization
+        // Use this for initialization
 	void Start () {
-        //Player取得
-        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>(); 
-        this.initParmeter();
+        #region  ゲージのスケール情報
+        hoge = GameObject.Find("Image").GetComponent<RectTransform>();
+        hoge.localScale = new Vector3(3, 0.5f, 1);
+        #endregion
         
-       
+        //player = GameObject.Find("Player").GetComponent<Player>();//Player取得
+        g_bar = GameObject.Find("Image").GetComponent<Image>();              
 	}
-	
+    float _gage = 1.0f;
 	// Update is called once per frame
 	void Update () {
-    
+        if (Input.GetKey(KeyCode.Space))
+        {
+            _gage -= 0.01f;
+
+        }
+        else if (Input.GetKeyUp(KeyCode.Space))
+        {
+            _gage = 1f;
+        }
+        if (_gage < 0.0f)
+        {
+            _gage = 1.0f;
+        }
+        g_bar.fillAmount= _gage; 
+           
 	}
-
-    private void initParmeter()
-    {
-        //g_bar = gameObject.Find("a").GetComponent<Image>();
-
-        g_bar.fillAmount = 100;
-    }
-    
 }
--- Assets/Suzuki/Item/Program/item.cs
+++ Assets/Suzuki/Item/Program/item.cs
@@ -1,23 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+using UnityEngine.UI;
 public class item : MonoBehaviour {
-
-
-   public float  i_speed;
+    public Image _speed;
     // Use this for initialization
     void Start () {
-	
+        
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        transform.Rotate(new Vector3(1.0f, 1.0f, 0.01f)*i_speed);
+        transform.Rotate(new Vector3(1.0f, 1.0f, 0.01f));
+        transform.Translate(0.01f, 0.004f, 0.02f); 
+
 	}
 
-    public void OnTriggerEnter2D(Collision2D collision)
+    public void OnTriggerEnter2D(Collider2D collider)
     {
-        var _target = collision.gameObject;
+        
+        var _target = collider.gameObject;
         if (_target.tag == "Player")
         {
             Destroy(gameObject);
